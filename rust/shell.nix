f9cd90c422f4b157522212a90686e0732ac58132
rafaelcgs10 <rafaelcgs10@gmail.com>
2022-01-14 10:04:54 -0300

    Fix version

--- rust/shell.nix
+++ rust/shell.nix
@@ -1,6 +1,6 @@
 let
   pkgs = import
-    (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/93184413f2180ce79d53df91df4d43c2e8f931aa.tar.gz) { };
+    (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/386234e2a61e1e8acf94dfa3a3d3ca19a6776efb.tar.gz) { };
   rustPackages = [
     pkgs.cargo
     pkgs.rustc
